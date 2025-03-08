0bdd972aec074e2ef31d39372085b12f329578d1
Peddanolla Ragichettukindi Vishal <prvishal200416@gmail.com>
2025-03-08 18:56:41 +0530

    first commit

---
+++ client/src/pages/home.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
+import './home.css';
+
+export const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("https://mern-recipe-app1-server.onrender.com/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    const fetchSavedRecipes = async () => {
+      try {
+        const response = await axios.get(
+          `https://mern-recipe-app1-server.onrender.com/recipes/savedRecipes/ids/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchRecipes();
+    fetchSavedRecipes();
+  }, []);
+
+  const saveRecipe = async (recipeID) => {
+    try {
+      const response = await axios.put("https://mern-recipe-app1-server.onrender.com/recipes", {
+        recipeID,
+        userID,
+      });
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const isRecipeSaved = (id) => savedRecipes.includes(id);
+
+  return (
+    <div>
+    <h1>🍽️The Ultimate Recipe Sharing Platform</h1>
+    <p >
+  <strong>FlavorFusion</strong> is an innovative and dynamic recipe-sharing platform designed for food enthusiasts, 
+  home cooks, and professional chefs alike. It provides a seamless experience for users to <strong>create, share, and manage </strong> 
+  their favorite recipes while ensuring real-time updates across all views for a smooth and engaging interaction.
+</p>
+<br/><br/>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              <button
+                onClick={() => saveRecipe(recipe._id)}
+                disabled={isRecipeSaved(recipe._id)}
+              >
+                {isRecipeSaved(recipe._id) ? "Saved" : "Save"}
+              </button>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
